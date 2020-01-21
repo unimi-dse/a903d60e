29d0064a57082ae1c3687a3aa135480930f9ecc5
Kafkalab <59999184+Kafkalab@users.noreply.github.com>
2020-01-21 09:44:56 +0100

    Update Nationality.R

--- R/Nationality.R
+++ R/Nationality.R
@@ -1,3 +1,11 @@
 #R Package to analyze data
-
+#installation
 install.packages("readxl")
+
+#usage
+library(readxl)
+xlsx_example <- readxl_example("datasets.xlsx")
+read_excel(xlsx_example)
+
+#List the sheet names with excel_sheets().
+excel_sheets(xlsx_example)
