018d8bd7008d3ea713a55f27d6dbe0798c93b7bd
Kafkalab <59999184+Kafkalab@users.noreply.github.com>
2020-01-21 09:55:40 +0100

    Update Nationality.R

--- R/Nationality.R
+++ R/Nationality.R
@@ -15,3 +15,22 @@ excel_sheets(xlsx_example)
 
 #The above R code, assumes that the file “my_file.xls” and “my_file.xlsx” is in your current working directory. 
 #To know your current working directory, type the function getwd() in R console.
+
+# Hello, world!
+#
+# This is an example function named 'hello' 
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
