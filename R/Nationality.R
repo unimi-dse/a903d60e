a89ad3fa6e5b39334918c1ddb485a6088e9c54b5
Kafkalab <59999184+Kafkalab@users.noreply.github.com>
2020-01-23 12:23:51 +0100

    Update Nationality.R

--- R/Nationality.R
+++ R/Nationality.R
@@ -1,19 +1,25 @@
+#This should return TRUE in the console if your Rtools installation worked properly
+find_rtools()
+
+#INSTALLATION OF ALL THE PACKAGES NEEDED, REMINDER FOR USE TO ALREADY INSTALL RTOOL before
+
 install.packages("httr")
-install.packages("Rtools")
+install.packages("usethis")
+install.packages("devtools")
 install.packages("thePackage")
-#install.package("devtools")
-install.packages("tidyverse")
-install.packages("plotly")
 
-
+#NEED TO CALL THE LIBRARIES TO USE THE FUNCTION PROPERLY
 library(httr)
+library(usethis)
 library(thePackage)
-library(Rtools)
-#library(devtools)
+#library(Rtools)
+library(devtools)
 library(tidyverse)
 library(plotly)
 
-#import data
+
+
+#IMPORT DATA
 X <- read.csv("https://raw.githubusercontent.com/unimi-dse/a903d60e/master/R/Data.csv")
 
 colnames(X) <- c("COUNTRIES","NUMBER OF VISITORS")
@@ -23,12 +29,197 @@ X
 #p <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
 #p
 
+#BEFORE USING PLOTLY, CHECK THE VERSION OF THE PACKAGE INSTALLED
 library(plotly)
 packageVersion('plotly')
 
-#data visualization
+#DATA VISUALIZATION WITH PLOTLY
 p <- plot_ly(
   x = c("Austria",
+        "Belgio",
+        "Danimarca",
+        "Finlandia",
+        "Francia",
+        "Germania",
+        "Grecia",
+        "Irlanda",
+        "Lussemburgo",
+        "Paesi Bassi",
+        "Portogallo",
+        "Regno unito",
+        "Spagna",
+        "Svezia",
+        "Bulgaria",
+        "CecaRepubblica",
+        "Cipro",
+        "Croazia",
+        "Estonia",
+        "Lettonia",
+        "Lituania",
+        "Malta",
+        "Norvegia",
+        "Polonia",
+        "Romania",
+        "Russia",
+        "Slovacchia",
+        "Slovenia",
+        "Turchia",
+        "Ungheria",
+        "Islanda",
+        "Altri paesi europei",
+        "Egitto",
+        "NordAfrica",
+        "Sud Africa",
+        "Altri paesi africani",
+        "Israele",
+        "AltriMedioOriente",
+        "Cina",
+        "Giappone",
+        "Corea del sud",
+        "India",
+        "Altri Paesi dell'Asia",
+        "Canada",
+        "Stati Uniti",
+        "Messico",
+        "Argentina",
+        "Brasile",
+        "Venezuela",
+        "Altri Paesi Nordamericani",
+        "Altri Paesi dell'America centro-meridionale",
+        "Nuova Zelanda",
+        "Altri Paesi dell'Oceania",
+        "Altri",
+        "Switzerland and Liechtenstein",
+        "Australia"),
+  y = c(9519898,
+        4912441,
+        3296288,
+        934276,
+        14197976,
+        58645390,
+        852765,
+        1734636,
+        387452,
+        11037222,
+        1005014,
+        14043501,
+        5684438,
+        2566196,
+        711441,
+        3903499,
+        97618,
+        697665,
+        216920,
+        261615,
+        454214,
+        373446,
+        1285585,
+        5743920,
+        2510265,
+        5382255,
+        1144272,
+        890307,
+        985097,
+        2028427,
+        142553,
+        3746661,
+        285933,
+        606081,
+        303267,
+        780323,
+        1732936,
+        1454621,
+        5287714,
+        2238222,
+        1843291,
+        1258210,
+        2568242,
+        2398572,
+        14546868,
+        818997,
+        1752110,
+        2546197,
+        137147,
+        75297,
+        1664748,
+        359063,
+        107875,
+        877186,
+        10679102,
+        2795291),
+  name = "Nationality and N. of Visitors",
+  type = "box"
+)
+
+p
+
+#DATA ANALYSIS (MODE, MEDIAN,RANGE)
+
+
+
+#FUNCTION OF NATIONALITY (FIND THE MAX. NUMBER)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#install.packages("httr")
+#install.packages("Rtools")
+#install.packages("thePackage")
+#install.package("devtools")
+#install.packages("tidyverse")
+#install.packages("plotly")
+
+
+#library(httr)
+#library(thePackage)
+#library(Rtools)
+#library(devtools)
+#library(tidyverse)
+#library(plotly)
+
+#import data
+#X <- read.csv("https://raw.githubusercontent.com/unimi-dse/a903d60e/master/R/Data.csv")
+
+#colnames(X) <- c("COUNTRIES","NUMBER OF VISITORS")
+
+#X
+
+#p <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
+#p
+
+#library(plotly)
+#packageVersion('plotly')
+
+#data visualization
+#p <- plot_ly(
+#  x = c("Austria",
           "Belgio",
           "Danimarca",
           "Finlandia",
@@ -84,7 +275,7 @@ p <- plot_ly(
           "Altri",
           "Switzerland and Liechtenstein",
           "Australia"),
-  y = c(9519898,
+  #y = c(9519898,
           4912441,
           3296288,
           934276,
@@ -140,11 +331,11 @@ p <- plot_ly(
           877186,
           10679102,
           2795291),
-  name = "SF Zoo",
-  type = "N/C"
-)
+#  name = "SF Zoo",
+#  type = "N/C"
+#)
 
-p
+#p
 
 
 #data analysis
