288f69b16f39db6584b4b9fd4bcf2846a8fdfa3c
Kafkalab <59999184+Kafkalab@users.noreply.github.com>
2020-01-30 21:05:30 +0100

    Update hello

--- R/hello.R
+++ R/hello.R
@@ -1,11 +1,19 @@
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
-install.packages("nation")
+install.packages("devtools")
+devtools::install_github('unimi-dse/a903d60e')
+#install.packages("nation")
 
 install.packages('quantmod')
+install.packages("plotly")
+
+library(plotly)
 
 require(quantmod)
 
-x <- getSymbols(Symbols = 'FB', src = 'yahoo', auto.assign = FALSE)
+x <- getSymbols(Symbols = 'EUR/TND', src = 'oanda', auto.assign = FALSE)
 tail(x)
+
+plot(x[,1])
+
