c24234ca5f1094f9359a9e026be6de9a3a50b9e8
Kafkalab <59999184+Kafkalab@users.noreply.github.com>
2020-01-30 20:44:36 +0100

    Update R

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,13 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+install.packages('quantmod')
+
+require(quantmod)
+
+x <- getSymbols(Symbols = 'EUR/TND', src = 'oanda', auto.assign = FALSE)
+
+x
+
+plot(x[,1])
